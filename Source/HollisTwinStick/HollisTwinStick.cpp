83dbf747eced1f941491dadf205d07b897821716
Captain7luffy <70959354+Captain7luffy@users.noreply.github.com>
2020-11-24 10:49:11 -0500

    Pushone

---
+++ Source/HollisTwinStick/HollisTwinStick.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "HollisTwinStick.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HollisTwinStick, "HollisTwinStick" );
